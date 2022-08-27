1414212ba707ebcbc6ee04cc9c78d85441a30a67
franranieli <franranieli@gmail.com>
2022-08-26 22:05:16 -0300

    modificacion de rutas app.js

--- app.js
+++ app.js
@@ -38,4 +38,24 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/register.html', (req, res)=>{
+  res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/serviceDetail.html', (req, res)=>{
+  res.sendFile(__dirname + '/views/serviceDetail.html');
+});
+
+app.get('/carrito.html', (req, res)=>{
+  res.sendFile(__dirname + '/views/carrito.html');
+});
+
+app.get('/login.html', (req, res)=>{
+  res.sendFile(__dirname + '/views/login.html');
+});
+
 module.exports = app;
